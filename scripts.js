0fdb27200f76492e1c7ecb0fbd3d522fdbfc3a34
Gabor Orban <orbangabeszb@gmail.com>
2020-04-21 16:04:43 +0200

    more photo objects created

--- scripts.js
+++ scripts.js
@@ -9,7 +9,9 @@ function createPhotoData(photo, title, description) {
 
 let viraglada = createPhotoData('Photos/viraglada.jpg', 'Virágláda', 'Fűszernövények egy ládában, melyeket magam ültettem.');
 let kokapu = createPhotoData('Photos/kokapu.jpg', 'Kőkapu', 'A Badacsony híres útja. Magam készítettem, szúnyogokkal terrorizálva.');
-
+let gyomber = createPhotoData('Photos/gyomber', 'Gyömbér-csúcs', 'A Gyömbér, az Alacsony-Tátra legmagasabb csúcsa kibukkan a felhők közül. A kép a Chopokról készült.');
+let jegkristaly = createPhotoData('Photos/jegkristalyok', 'Jégkristályok', 'A még zöld füvön keletkezett nagy jégkristályok a Mátrában.');
+let zsalya = createPhotoData('Photos/zsalya', 'Zsálya a naplementében', 'A kép az Alpokban készült, a Wolfgang See mellett eső utáni sétán.')
 
 $('#view-img').attr('src', viraglada.photo);
 $('#photo-title').text(viraglada.title);
