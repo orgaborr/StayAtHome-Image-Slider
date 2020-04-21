f337eee4618d907fbff80b8b7f44a3b62d785596
Gabor Orban <orbangabeszb@gmail.com>
2020-04-21 15:16:51 +0200

    view content dinamically added

--- scripts.js
+++ scripts.js
@@ -3,3 +3,7 @@ let data = {
     title: 'Virágláda',
     description: 'Fűszernövények egy ládában, melyeket magam ültettem.'
 };
+
+$('#view-img').attr('src', data.photo);
+$('#photo-title').text(data.title);
+$('#photo-description').text(data.description);
