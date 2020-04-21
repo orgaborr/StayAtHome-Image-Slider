a328ac1c0d7eb54aa099532c39b3028f3f0e6178
Gabor Orban <orbangabeszb@gmail.com>
2020-04-21 16:55:32 +0200

    image moved to middle of the viewer

--- scripts.js
+++ scripts.js
@@ -9,11 +9,14 @@ function createPhotoData(photo, title, description) {
 
 let viraglada = createPhotoData('Photos/viraglada.jpg', 'Virágláda', 'Fűszernövények egy ládában, melyeket magam ültettem.');
 let kokapu = createPhotoData('Photos/kokapu.jpg', 'Kőkapu', 'A Badacsony híres útja. Magam készítettem, szúnyogokkal terrorizálva.');
-let gyomber = createPhotoData('Photos/gyomber', 'Gyömbér-csúcs', 'A Gyömbér, az Alacsony-Tátra legmagasabb csúcsa kibukkan a felhők közül. A kép a Chopokról készült.');
-let jegkristaly = createPhotoData('Photos/jegkristalyok', 'Jégkristályok', 'A még zöld füvön keletkezett nagy jégkristályok a Mátrában.');
-let zsalya = createPhotoData('Photos/zsalya', 'Zsálya a naplementében', 'A kép az Alpokban készült, a Wolfgang See mellett eső utáni sétán.')
+let gyomber = createPhotoData('Photos/gyomber.jpg', 'Gyömbér-csúcs', 'A Gyömbér, az Alacsony-Tátra legmagasabb csúcsa kibukkan a felhők közül. A kép a Chopokról készült.');
+let jegkristaly = createPhotoData('Photos/jegkristalyok.jpg', 'Jégkristályok', 'A még zöld füvön keletkezett nagy jégkristályok a Mátrában.');
+let zsalya = createPhotoData('Photos/zsalya.jpg', 'Zsálya a naplementében', 'A kép az Alpokban készült, a Wolfgang See mellett eső utáni sétán.');
 
-$('#view-img').attr('src', viraglada.photo);
-$('#photo-title').text(viraglada.title);
-$('#photo-description').text(viraglada.description);
+let images = [viraglada, kokapu, gyomber, jegkristaly, zsalya];
+
+let photoDisplayed = 4;
+$('#view-img').attr('src', images[photoDisplayed].photo);
+$('#photo-title').text(images[photoDisplayed].title);
+$('#photo-description').text(images[photoDisplayed].description);
 
